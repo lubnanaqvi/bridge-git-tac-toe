f3b048d4b6dad6df49b5b8740c0e745cd301d36d
Ashley <ashleyhellyer@gmail.com>
2019-02-05 19:39:43 -0500

    ashley move 3

--- src/yourMoves.js
+++ src/yourMoves.js
@@ -1,6 +1,6 @@
 export const x = "❌";
 export const o = "⭕️";
 
-const moves = [[[x], [], [x]], [[], [o], []], [[], [], [o]]];
+const moves = [[[x], [], [x]], [[], [o], []], [[x], [], [o]]];
 
 export default moves;
