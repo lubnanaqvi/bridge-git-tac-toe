1bfee0876282d17fa50f66f79049d63cb7631d85
Lubna Naqvi <lubnanaqvi11@gmail.com>
2019-02-05 19:38:22 -0500

    changes in ashley's move

--- src/yourMoves.js
+++ src/yourMoves.js
@@ -1,6 +1,6 @@
 export const x = "❌";
 export const o = "⭕️";
 
-const moves = [[[x], [x], [x]], [[], [o], []], [[], [], [o]]];
+const moves = [[[x], [], [x]], [[], [o], []], [[], [], [o]]];
 
 export default moves;
